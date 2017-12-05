0e1b4478628eac0a0be5b717899e259ac1cfadd6
knish <knishimuta@udallas.edu>
2017-12-05 15:41:43 -0600

    Adding new version of MazeWriter

--- src/adsfinal/MazeWriter.java
+++ src/adsfinal/MazeWriter.java
@@ -1,3 +1,6 @@
+// Kyle Nishimuta
+
+
 package adsfinal;
 
 import java.util.ArrayList;
@@ -5,6 +8,10 @@ import java.util.ArrayList;
 public class MazeWriter {
 
 	static void  buildMaze(int[][][] b) {
+		/*
+		 * IMPORTANT --> Written based on the assumption that the maze is square
+		 */
+
 		/*
 		 * Builds maze using initial maze as base:
 		 *  - Choose random vertex in maze that is not 
@@ -17,6 +24,25 @@ public class MazeWriter {
 		 */
 
 		ArrayList<Integer> unusedVertices = new ArrayList<Integer>();
+
+		// Fill unusedVertices with the unused vertices in the maze
+		int numItems = 0;
+		for(int i = 0; i < b.length; i++) {
+			for(int j = 0; j < b[i].length; j++) {
+				unusedVertices.add(numItems);
+				System.out.println("Added " + numItems + " to list of vertices");
+				numItems++;
+			}
+		}
+		
+		// Remove vertices that are on edges
+		double verticesDouble = numItems;
+		for(int i = 0;) {
+			
+		}
+			
+		// Make first wall
+		
 		
 		
 
